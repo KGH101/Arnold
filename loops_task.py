3aa4440e098437300221e7b32a27df47820e51b7
KGH101 <daviesarnold99@gmail.com>
2024-10-30 16:50:18 +0300

    update

--- loops_task.py
+++ loops_task.py
@@ -7,54 +7,97 @@ print(numbers)
 
 # From 1 above display the ones divisible by 7 or 5 inside a list.
 
-numbers = list(range(1,51))
+divisible_seven_5=[]
 for i in numbers:
     if i%7==0 or i%5==0:
-        print(i)
+        divisible_seven_5.append(i)
+
+
+print(divisible_seven_5)
 
 # Find sum and average of values in the range between 10 to 40.
 
 numbers = list(range(10,41))
-total_sum = sum(numbers)
-average = total_sum / len(numbers)
+sum=0
+count=0
 
-print(f"Sum: {total_sum}")
-print(f"Average: {average}")
+for i in numbers:
+    sum+=i
+average=sum / len(numbers)    
+print(sum)    
+print(average)
+#total_sum = sum(numbers)
+#average = total_sum / len(numbers)
+
+#print(f"Sum: {total_sum}")
+#print(f"Average: {average}")#
 
 
 # Put in a list the first 10 odd numbers between 10 to 50.
 
 
-numbers = range(10,51)
+numbers = list(range(10,50))
 odd=[]
 for i in numbers:
     if i%2!=0:
         odd.append(i)
     if len(odd)==10:
         break
-    print(odd)    
+print(odd)   
+
+    # or
+
+numbers = list(range(10,50))
+odd=[]   
+count=0
+
+for i in numbers:
+    if i%2!=0:
+        odd.append(i)
+        count+=1
+        if count==10:
+            break
+
+print(odd)
+    
+
 
 
 # write a program that takes a number as input and prints its
 # multiplication table up to 10 using a for loop.
 
 number = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(f"{number} x {i} = {number * i}")
+for i in range(0, 11):
+    mult=number*i
+    print(f"{number} x {i} = {mult}")
 
 
 # write a program that counts and prints the number of even numbers
 # between 1 and 50 using a for loop
+
+count_even = 0
+
+# Loop through numbers from 1 to 50
+for i in range(1, 51):
+    # Check if the number is even
+    if i % 2 == 0:
+        count_even += 1
+
+# Print the total count of even numbers
+print(f"The number of even numbers between 1 and 50 is: {count_even}", count_even)
 
 
 # ls1 = [ (“Jay”, 20), (“Mo”, 30), (“Mya”, 32) ]
 # Display the total quantity of the 3 above.
 
 
-s1 = [("Jay", 20), ("Mo", 30), ("Mya", 32)]
+ls1 = [("Jay", 20), ("Mo", 30), ("Mya", 32)]
 
-# Calculate the total quantity
-total_quantity = sum(quantity for name, quantity in s1)
+total_quantity = 0
 
-# Display the total quantity
-print("Total quantity:", total_quantity)
+
+for i in ls1:
+    stock=i[1]
+    total_quantity = total_quantity + stock # total_quantity += item[1]
+
+print(f"Total Quantity: {total_quantity}", )
